3f13b3e2d76d4a047be8c349be3e7e222a2b2557
Erin Cathignol <erin.cathignol@recia.fr>
2024-06-24 08:34:45 +0200

    FIX - suppression index copy.html

--- gen-new-adoption-form.sh
+++ gen-new-adoption-form.sh
@@ -1,3 +1,3 @@
-read -p "Renseigez le nom de l'animal ou des animaux concernés par cette fiche séparés par des tirets (-) : " name
+read -p "Renseignez le nom de l'animal ou des animaux concernés par cette fiche séparés par des tirets (-) : " name
 touch fiches-adoption/a-$name.html
 code fiches-adoption/a-$name.html
